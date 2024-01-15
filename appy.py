cb1ab32d1287beb345092ea9ac290a1829dcd70d
Fahri31-cegit config --global user.name Fahri31-ce <muhammadfahry3107@gmail.com>
2024-01-15 12:38:36 +0700

    Firs Github Project

---
+++ appy.py
@@ -0,0 +1,8 @@
+def luas_segitiga():
+    a = int(input("Masukan alas segitiga:"))
+    t = int(input("masukkan tinggi segitiga:"))
+    luas = a * t / 2
+    print("luas segitiga adalah:", luas)
+
+# Panggil fungsi di luar definisi
+luas_segitiga()
